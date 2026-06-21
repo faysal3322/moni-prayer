54cae5891b5757ec50ce632bb118fe3af634ef27
faysal3322 <281077321+faysal3322@users.noreply.github.com>
2026-06-21 16:11:56 +0600

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/moni_prayer/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/moni_prayer/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.example.moni_prayer
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import es.antonborri.home_widget.HomeWidgetPlugin
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        flutterEngine.plugins.add(HomeWidgetPlugin())
+    }
+}

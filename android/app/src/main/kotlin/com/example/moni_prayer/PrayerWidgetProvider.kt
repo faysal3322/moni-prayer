b1a930b284d3704eb995104881673e0e69376465
faysal3322 <281077321+faysal3322@users.noreply.github.com>
2026-08-15 04:38:10 +0530

    Update PrayerWidgetProvider.kt

--- android/app/src/main/kotlin/com/example/moni_prayer/PrayerWidgetProvider.kt
+++ android/app/src/main/kotlin/com/example/moni_prayer/PrayerWidgetProvider.kt
@@ -111,6 +111,8 @@ class PrayerWidgetProvider : AppWidgetProvider() {
         views.setTextViewText(R.id.widget_gregorian, prefs.getString("widget_gregorian", "") ?: "")
         views.setTextViewText(R.id.widget_hijri, prefs.getString("widget_hijri", "") ?: "")
         views.setTextViewText(R.id.widget_bangla_date, prefs.getString("widget_bangla_date", "") ?: "")
+        views.setTextViewText(R.id.widget_waqt_name, prefs.getString("widget_waqt_name", "") ?: "")
+        views.setTextViewText(R.id.widget_waqt_range, prefs.getString("widget_waqt_range", "") ?: "")
         views.setTextViewText(R.id.widget_sunrise, prefs.getString("widget_sunrise", "") ?: "")
         views.setTextViewText(R.id.widget_sunset, prefs.getString("widget_sunset", "") ?: "")
         views.setTextViewText(R.id.widget_sehri, prefs.getString("widget_sehri", "") ?: "")
